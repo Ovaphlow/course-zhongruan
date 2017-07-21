60c54933e1434afc8fdee408624f304d2d7e2b4c
ovaphlow <jsfreecss@163.com>
2017-07-21 11:28:38 +0800

    parse token

--- sign-in-center/src/main/java/com/zhongruan/sic/controller/MainController.java
+++ sign-in-center/src/main/java/com/zhongruan/sic/controller/MainController.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
@@ -25,6 +26,33 @@ public class MainController {
 
   Logger logger = LoggerFactory.getLogger(ApiController.class);
 
+  @GetMapping(value = "info")
+  public Map<String, Object> info(@RequestHeader("Authorization") String auth) {
+    logger.info("auth: {}", auth);
+    Map<String, Object> result = new HashMap<String, Object>();
+    if (auth == null || !auth.startsWith("Bearer ")) {
+      result.put("statusCode", 401);
+      return result;
+    }
+    final String token = auth.substring(7);
+    logger.info("token: {}", token);
+    try {
+      final Claims claims = Jwts.parser()
+          .setSigningKey(Config.JWT_SECRET_KEY)
+          .parseClaimsJws(token)
+          .getBody();
+      logger.info("claims: {}", claims);
+      logger.info("sub: {}", claims.get("sub"));
+    }
+    catch (Exception e) {
+      e.printStackTrace();
+      result.put("statusCode", 403);
+      return result;
+    }
+    result.put("statusCode", 200);
+    return result;
+  }
+
   @GetMapping(value = "sign-in")
   public String testSignIn() {
     return "testing: OK";
@@ -36,8 +64,9 @@ public class MainController {
     logger.info("Sign in: {}", user);
     String token = Jwts.builder()
       .setSubject(user.getAccount())
-      .claim("role", "用户")
       .setIssuedAt(new Date())
+      // .setExpiration()
+      .claim("role", "用户")
       .signWith(SignatureAlgorithm.HS256, Config.JWT_SECRET_KEY)
       .compact();
     Map<String, String> result = new HashMap<String, String>();
