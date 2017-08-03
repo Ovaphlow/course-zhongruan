b91a0884d83782d5efce2c222b0d0eadf1b31588
ovaphlow <jsfreecss@163.com>
2017-08-03 11:26:35 +0800

    .

---
+++ book/src/main/java/controller/IndexController.java
@@ -0,0 +1,41 @@
+package controller;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.codec.digest.DigestUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import dao.UserDao;
+
+@Controller
+public class IndexController {
+
+  private final Logger logger = LoggerFactory.getLogger(IndexController.class);
+
+  @Autowired
+  private UserDao userDao;
+
+  @RequestMapping(value="/", method=RequestMethod.GET)
+  public String index() {
+    return "index";
+  }
+
+  @RequestMapping(value="/book/{id}", method=RequestMethod.GET)
+  @ResponseBody
+  public Map<String, Object> book(
+    @PathVariable("id") int id
+  ) {
+    logger.debug("id: {}", id);
+    Map<String, Object> result = userDao.findById(id);
+    logger.debug("result: {}", result);
+    return result;
+  }
+}
